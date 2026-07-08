3b927c2027fdbb7423950ff4d23b4e2d27db0238
Victor Abreu <victorabreuf13@gmail.com>
2026-07-08 13:09:42 -0300

    feat: wire load-silver CLI command for GTFS

--- src/opa_database/cli.py
+++ src/opa_database/cli.py
@@ -5,6 +5,7 @@ import click
 from opa_database.adapters import afc, avl, gtfs, vehicle_dictionary
 from opa_database.silver import afc as silver_afc
 from opa_database.silver import avl as silver_avl
+from opa_database.silver import gtfs as silver_gtfs
 
 _ADAPTERS = {
     "afc": afc,
@@ -19,6 +20,7 @@ _REFERENCE_ADAPTERS = {
 _SILVER_LOADERS = {
     "afc": silver_afc,
     "avl": silver_avl,
+    "gtfs": silver_gtfs,
 }
 
 
