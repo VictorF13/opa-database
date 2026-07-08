a8a10134339244960a992fb18ef49fc8803e369f
Victor Abreu <victorabreuf13@gmail.com>
2026-07-08 15:37:23 -0300

    feat: add GTFS trip/stop/stop_time gold models

---
+++ gold/tests/dim_gtfs_stop_time_trip_exists.sql
@@ -0,0 +1,6 @@
+-- dbt singular test: passes if this returns zero rows.
+select st.feed_version_date, st.trip_id, st.stop_sequence
+from {{ ref('dim_gtfs_stop_time') }} as st
+left join {{ ref('dim_gtfs_trip') }} as t
+    on t.feed_version_date = st.feed_version_date and t.trip_id = st.trip_id
+where st.trip_id is not null and t.trip_id is null
